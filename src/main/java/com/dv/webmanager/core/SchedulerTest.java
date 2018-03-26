8b0d25e55ad083a95a874d44b98e7ade9f3784fd
nicola <ichea88@gmail.com>
2018-03-26 23:20:52 +0200

    primo test con scheduling dei task;

---
+++ src/main/java/com/dv/webmanager/core/SchedulerTest.java
@@ -0,0 +1,28 @@
+package com.dv.webmanager.core;
+
+public class SchedulerTest {
+
+    public void testPrint() {
+        System.out.println("prova");
+    }
+
+//    private TaskExecutor executor;
+//
+//    public SchedulerTest() {
+//        executor = null;
+//    }
+//
+//    public SchedulerTest(TaskExecutor te) {
+//        executor = te;
+//    }
+//
+//    public void callMethod() {
+//        executor.execute(new Runnable() {
+//            public void run() {
+//                System.out.println("prova");
+//            }
+//        });
+//    }
+//
+
+}
